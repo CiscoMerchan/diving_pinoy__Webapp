536c55340399544f48df1b6f68464bd4736c08bb
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-12-20 22:39:45 +1000

    Ad start of Routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import './index.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar, Partners} from './components';
 import { Header, Dive, About, Footer } from './container';
 import Blog from './container/blog/Blog';
@@ -20,6 +21,13 @@ function App() {
     <div className='bar'/>
     <Blog />
     <div className='bar'/>
+    {/* <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dive" element={<Dive />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes> */}
     <Footer />
     
     </div>
