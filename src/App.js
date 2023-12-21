8f7be975cbcd5b229eae113983c479e795f13fa5
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-12-21 22:38:47 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import './index.css'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Routes, Route } from "react-router-dom";
 import Home from './pages/Home/Home';
 import { Navbar } from './components';
 import { Header, Footer } from './container';
-import Blog from './container/blog/Blog';
+
 
 
 function App() {
