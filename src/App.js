05ad718d465bb4b2d77825795ff2626fb6070276
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-12-22 12:51:58 +1000

    Add Dive page path

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import './index.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from './pages/Home/Home';
 import Navbar from './components/navbar/Navbar';
 import { Header, Footer } from './container';
+// Pages
+import Home from './pages/Home/Home';
+import Dive from './pages/Dive/Dive';
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
       
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/dive' element={<Dive />} />
           
         </Routes>
       <div className='bar'/>
