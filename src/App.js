f2194eaee73877385fdb04df9eb00c6ac41728c1
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-12-22 10:36:56 +1000

    Home page working

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import './index.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './pages/Home/Home';
-import { Navbar } from './components';
+import Navbar from './components/navbar/Navbar';
 import { Header, Footer } from './container';
 
 function App() {
@@ -11,12 +11,11 @@ function App() {
       <Navbar />
       <div className='bar'/>
       <Header />
-      <Router>
+      
         <Routes>
           <Route path='/' element={<Home />} />
           
         </Routes>
-      </Router>
       <div className='bar'/>
       <Footer />
     </div>
