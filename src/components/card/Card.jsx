29ffd31de756f37421da88d3232132fe9142b679
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-12-23 14:13:26 +1000

    Card.jsx component

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 import './Card'
-const Card = () => {
+const Card = ({ image_card, alt_image_card, title_card, text_card, btn_cta_card}) => {
   return (
-    <div>Card</div>
+    <>
+    <div className='webapp__card'>
+        <div className='webapp__card-imagen'>
+            <img src={image_card} alt={alt_image_card}/>
+        </div>    
+        <div className='webapp__card-container'>
+            <div className='webapp__card-container_title'>
+                <h3>{title_card}</h3>
+            </div>
+            <div className='webapp__card-container_text'>
+                <p>{text_card}</p>
+            </div>
+        </div>
+        <div className='webapp__card-btn_cta'>
+            {btn_cta_card}
+        </div>
+        
+    </div>
+        
+    </>
   )
 }
 
