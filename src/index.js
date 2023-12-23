325001ce20fa1353e7614300f9b680b2f7cca444
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-12-23 19:28:03 +1000

    Debug by adding <StrictMode> in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { StrictMode } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-    <App />
-  </Router>
+  <StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </StrictMode>
 );
