014909fae587a3dcef79b001eba87120ced55399
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-12-23 15:04:57 +1000

    Course.jsx component and  Add map for Card component

--- src/container/course/Course.jsx
+++ src/container/course/Course.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import 'Course.css'
+import Card from '../../components/card/Card'
 const Course = () => {
   return (
-    <div>Course</div>
+    <div className='webapp__course'>
+        <div className='webapp__course-title'>
+            <h2>{title_course}</h2>
+        </div>
+        <div className='webapp__course-container'>
+            {/* Card component to display the courses */}
+            {/* Map over courseData JSON file and render a Card component for each item */}
+            {courseData.map((cardData, index) => (
+            <Card key={index} {...cardData} />
+            ))}
+        </div>
+    </div>
   )
 }
 
