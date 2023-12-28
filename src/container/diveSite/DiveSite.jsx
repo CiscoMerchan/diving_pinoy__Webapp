549ad48ef3655c7cc4aff7461d7f29fb7e661d46
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-12-28 17:53:37 +1000

    Add Carousel component to DiveSite

--- src/container/diveSite/DiveSite.jsx
+++ src/container/diveSite/DiveSite.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './DiveSite.css'
 import FlipCard from '../../components/flipCard/FlipCard'; 
+import Carousel from '../../components/carousel/Carousel';
 
 const DiveSite = ({ cardData }) => {
   return (
     
     <div className="webapp__divesite">
       {cardData.map((card) => (
-        <FlipCard key={card.id} images={card.images} description={card.description} />
+        <FlipCard key={card.id} 
+        images= {<Carousel images={card.images} />}
+        description={card.description} />
       ))}
     </div>
   );
