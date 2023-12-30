b3f6b1d1f55b5403accf76dff37f06a92e7bcbd1
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-12-30 15:50:23 +1000

    Update About page Structure

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,22 +1,32 @@
 import React from 'react'
 import './About.css'
+
 const About = () => {
-  return (
+  
+   return (
     <div className='webapp_about'>
         <div className='webapp_about-title'>
             <h1>About Boracay</h1>
+            <img src='' alt=""/>
         </div>
         <div className='webapp_about-container'>
             <div className='webapp_about-container_intro'>
                 <p></p>
+                <img src='' alt=""/>
             </div>
             <div className='webapp_about-container_land'>
                 <h2>Boracay on Land</h2>
-                <p></p>
+                <div>
+                    <img src='' alt=""/>
+                    <p></p>
+                </div>
             </div>
             <div className='webapp_about-container_UW'>
             <h2>Boracay UnderWater</h2>
+            <div>
+                <img src='' alt=""/>
                 <p></p>
+            </div>
             </div>
         </div>
     </div>
