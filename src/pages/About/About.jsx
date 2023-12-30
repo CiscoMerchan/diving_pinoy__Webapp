3efeadd3eac80f5ab6dba343412c61357312ff8d
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-12-30 15:24:50 +1000

    Add About page Structure

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 import './About.css'
 const About = () => {
   return (
-    <div>About</div>
+    <div className='webapp_about'>
+        <div className='webapp_about-title'>
+            <h1>About Boracay</h1>
+        </div>
+        <div className='webapp_about-container'>
+            <div className='webapp_about-container_intro'>
+                <p></p>
+            </div>
+            <div className='webapp_about-container_land'>
+                <h2>Boracay on Land</h2>
+                <p></p>
+            </div>
+            <div className='webapp_about-container_UW'>
+            <h2>Boracay UnderWater</h2>
+                <p></p>
+            </div>
+        </div>
+    </div>
   )
 }
 
