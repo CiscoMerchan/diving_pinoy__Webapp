d8a78dc3c5241500338b728e4e375d74f6d7fe78
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-12-31 23:01:33 +1000

    Add about text UW

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -9,9 +9,10 @@ const About = () => {
     Boracay is known all over the world for its white sand beach and turquoise waters.
     For those who wish to do other activities than diving, Boracay offers many activities, such as: kitesurfing, parasailing, kayaking, sailing, karting, golf, jet skiing, etc.
     It is also well known for its festive atmosphere and nightlife, with many bars and clubs of different atmospheres ; local and international culinary varieties ; and in regard to accommodation, there is something in everybody's budget.`,
-
-
+    underWater: `	Like the vast majority of islands in the Philippines, Boracay has a lot to offer in diving, from those who wish to discover the underwater world for the first time, to experienced divers. The high season is from the end of November to May, however the best period specifically for diving is between May and June, with an average water temperature between 29C ° and 30C °, and the visibility goes from 15m to over 30m.
+    Boracay has 12 dive sites in the west and 5 in the East (diving on the East side is only during  Habagat *).`
   }
+
    return (
     <div className='webapp_about'>
         <div className='webapp_about-title'>
@@ -34,7 +35,7 @@ const About = () => {
                 <h2>Boracay UnderWater</h2>
                 <div className='webapp_about-container_UW-container'>
                     <img src='' alt=""/>
-                    <p></p>
+                    <p>{text.underWater}</p>
                 </div>
             </div>
         </div>
