85decd4dc8a6362c71400d561703be643ca39de8
CiscoMerchan <franciscoeliasm@yahoo.com>
2024-01-02 14:11:17 +1000

    Add images for header

--- src/container/header/Header.jsx
+++ src/container/header/Header.jsx
@@ -4,6 +4,16 @@ import headerPic from '../../assets/img/headerPic.JPG'
 import padiResortIcon from '../../assets/img/icons/padiresort_icon.png'
 import logo from '../../assets/img/diving-pinoy-high-resolution-logo-color-on-transparent-background.png'
 const Header = () => {
+  // Random image for header
+  const randomImages = [
+    '../../assets/img/headerPic.JPG',
+    '/images/coursesImg/P3081028.JPG',
+    '/images/diveSite/angolPoint/angolP4.JPG',
+    '/images/diveSite/balinhai/b02.JPG',
+    '/images/funDive/P6270968.JPG',
+    '/images/diveSite/camiaII/camiaII02.JPG',
+    '/images/diveSite/punta_bunga/puntaB03.jpg'
+  ]
   return (
     <div className='web__app-header'>
       <img className='logo' src={logo} alt='logo'/>
