b432eb31d58edd8ba63e69a683f50bb7eb999568
CiscoMerchan <franciscoeliasm@yahoo.com>
2024-01-05 20:11:59 +1000

    Blogstructure page

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import './Blog.css'
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div>
+        <div>
+            {/* Last 3 Blog as a  */}
+            
+        </div>
+    </div>
   )
 }
 
