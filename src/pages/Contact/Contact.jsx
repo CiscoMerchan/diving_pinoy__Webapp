a7efac0f5589e641d05efcdccfa6344ba1297907
CiscoMerchan <franciscoeliasm@yahoo.com>
2024-01-06 22:48:17 +1000

    Update

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -6,10 +6,13 @@ import ContactForm from '../../components/contact_form/ContactForm'
 import { Box } from '@chakra-ui/react'
 const Contact = () => {
   return (
-    <>
+    <>  
+      <div className='webapp__contact'>
+        <h2>Contact Us ...</h2>
         <Box p={4}>
             <ContactForm />
         </Box>
+      </div>  
     </>
   )
 }
